4339e8e4c41c7b9fa9054bf24b54154725d0deaa
portable mickael <portable mickael@portablemickael.home>
2015-08-04 00:20:26 +0200

    Adding display frame without assets

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((640,480))
+
+print("Hello World");
+
+continuer = 1
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = 0
